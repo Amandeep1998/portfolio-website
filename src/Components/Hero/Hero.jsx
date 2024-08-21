e609f4461a6346127182483c9b0395691c3a9242
Apple <apple@Apples-MacBook-Air.local>
2024-08-21 15:22:41 +0530

    Added Resume Section

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -1,20 +1,36 @@
-import React from 'react'
-import './Hero.css'
-import profile_img from '../../assets/self-image.jpg'
-import AnchorLink from 'react-anchor-link-smooth-scroll'
+import React from "react";
+import "./Hero.css";
+import profile_img from "../../assets/self-image.jpg";
+import AnchorLink from "react-anchor-link-smooth-scroll";
 
 const Hero = () => {
+  let resumeUrl =
+    "https://drive.google.com/file/d/1w1CY_eC-hcavIWNJEEgzlU5MM10HRsOT/view?usp=drive_link";
+
   return (
-    <div id='home' className='hero'>
-      <img src={profile_img} alt="" className='profile-img' />
-      <h1><span>I'm Amandeep Saini,</span> frontend developer based in Navi Mumbai.</h1>
-      <p>I am a frontend developer from Navi Mumbai, India with 1.5+ years of experience.</p>
+    <div id="home" className="hero">
+      <img src={profile_img} alt="" className="profile-img" />
+      <h1>
+        <span>I'm Amandeep Saini,</span> frontend developer based in Navi
+        Mumbai.
+      </h1>
+      <p>
+        I specialize in creating stunning UI designs using React.js and Next.js,
+        with expert-level frontend development skills.
+      </p>
       <div className="hero-action">
         {/* <div className="hero-connect"><AnchorLink className='anchor-link' offset={50} href='#contact'>Connect with me</AnchorLink></div> */}
-        {/* <div className="hero-resume">My resume</div> */}
+        <div
+          onClick={() => {
+            window.open(resumeUrl, "_blank");
+          }}
+          className="hero-resume"
+        >
+          My Resume
+        </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Hero
+export default Hero;
